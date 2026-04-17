3f87b109f04cf3aa33a3f776442f09cc2d766ea8
Choppertree <isaiahasant@gmail.com>
2026-04-16 23:01:29 -0400

    starting to redesign UI, using claude dashboard as reference which islisted under FinanceDashboard, doing something similar in react where I separate each container/component in separate files for better editting and debugging

---
+++ Root.java
@@ -0,0 +1,20 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.table.*;
+/**
+ * main container of root
+ * @author Isaiah Santamaria
+ * @version 4/16/2025
+ */
+public class Root {
+    
+
+
+
+    
+}
---
+++ TransactionCont.java
@@ -0,0 +1,6 @@
+
+import javax.swing.*;
+public class TransactionCont extends JFrame{
+
+    
+}
